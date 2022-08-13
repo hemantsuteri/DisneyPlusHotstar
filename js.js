e5309f47ebd7361f1bfee2dd3ee959e47080d689
Hemant Suteri <hemantsuteri@gmail.com>
2022-08-13 12:54:55 +0530

    frontend

--- js.js
+++ js.js
@@ -1,87 +1,125 @@
 let movies = [
     {
-    name: "falcon and the winter soldier",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero maxime aliquam non! Nisi non voluptatem, eum nemo amet maiores quibusdam",
-    Image: "images/slider 2.PNG"
+      name: "WI  vs IND 317/8 (50)",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/slider 2.PNG",
     },
     {
-    name: "Loki",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero maxime aliquam non! Nisi non voluptatem, eum nemo amet maiores quibusdam", 
-    Image: "images/slider 1.PNG"
+      name: "Rudra: The Edge of Darkness",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/slider 1.PNG",
     },
     {
-        name: "Wanda Vision",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero maxime aliquam non! Nisi non voluptatem, eum nemo amet maiores quibusdam", 
-    Image: "images/slider 3.PNG"
+      name: "The Book of Boba Fett",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/slider 3.PNG",
     },
     {
-        name: "Raya and the last dragon",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero maxime aliquam non! Nisi non voluptatem, eum nemo amet maiores quibusdam", 
-    Image: "images/slider 4.PNG"
+      name: "raya and the last dragon",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/slider 4.PNG",
     },
     {
-        name: "Luca",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero maxime aliquam non! Nisi non voluptatem, eum nemo amet maiores quibusdam", 
-    Image: "images/slider 5.PNG"
-    }  
-];
-
-const caraousel = document.querySelector('.caraousel');
-let sliders=[];
-
-let slideIndex=0; //track the current slide
-
-const createSlide = () => {
-    // console.log('inside cs')
-    if(slideIndex >= movies.length){
-        slideIndex=0;
+      name: "luca",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/slider 5.PNG",
+    },
+  ];
+  
+  // const carousel = document.querySelector('.carousel');
+  let carousel = document.querySelector(".carousel");
+  let sliders = [];
+  
+  let slideIndex = 0; // to track current slide index.
+  
+  const createSlide = () => {
+    //   console.log("crete slide");
+    if (slideIndex >= movies.length) {
+      slideIndex = 0;
     }
-    // create DOM elements
-    let slide= document.createElement('div');
-    var imgElement= document.createElement('img');
-    let content= document.createElement('div');
-    let h1= document.createElement('h1');
-    let p= document.createElement('p');
-    
-    //attaching all elements
-    imgElement.appendChild(document.createTextNode(''));
+  
+    // creating DOM element
+    let slide = document.createElement("div");
+    let imgElement = document.createElement("img");
+    let content = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p");
+    // }
+  
+    // {
+    // attaching all elements
+    imgElement.appendChild(document.createTextNode(""));
     h1.appendChild(document.createTextNode(movies[slideIndex].name));
     p.appendChild(document.createTextNode(movies[slideIndex].des));
-    // p.appendChild(h1);
     content.appendChild(h1);
     content.appendChild(p);
-    // console.log(content)
+    slide.appendChild(content);
     slide.appendChild(imgElement);
-    caraousel.appendChild(slide);
-    console.log(slide)
-
-    // setting up images
-    imgElement.src = movies[slideIndex].Image;
-    // console.log(imgElement)
+    carousel.appendChild(slide);
+    // }
+    // {
+    // setting up image
+    imgElement.src = movies[slideIndex].image;
     slideIndex++;
-
-    //setting elements classnames
-    slide.className="slider";
-    content.className="slide-content";
-    h1.className="movie-title";
-    p.className="movie-des";
-
-    // console.log(slide);
+  
+    // setting elements classname
+    slide.className = "slider";
+    content.className = "slide-content";
+    h1.className = "movie-title";
+    p.className = "movie-des";
+  
     sliders.push(slide);
-
-    if(sliders.length) {
-        sliders[0].styles.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
-            30 * (sliders.length - 2)
-        }px)`;
+    // }
+    // {
+    // adding sliding effect
+    if (sliders.length) {
+      sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+        30 * (sliders.length - 2)
+      }px)`;
     }
-};
-
-for(let i= 0; i<3; i++){
+    // }
+  };
+  for (let i = 0; i < 3; i++) {
+    // console.log("inside for");
     createSlide();
-    // console.log('inside the loop')
-}
-
-setInterval(()=>{
+  }
+  
+  setInterval(() => {
     createSlide();
-},3000);
-// console.log('logging')
+  }, 4000);
+  /// video cards
+  
+  const videoCards = [...document.querySelectorAll(".video-card")];
+  
+  videoCards.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      let video = item.children[2];
+    //   console.log(video);
+      video.play();
+    });
+    item.addEventListener("mouseleave", () => {
+      let video = item.children[2];
+      console.log(video);
+      video.pause();
+    });
+  });
+  
+  // card sliders
+  
+  let cardContainers = [...document.querySelectorAll(".card-container")];
+  let preBtns = [...document.querySelectorAll(".pre-btn")];
+  let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+  
+  cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+  
+    nxtBtns[i].addEventListener("click", () => {
+      item.scrollLeft += containerWidth - 200;
+    });
+  
+    preBtns[i].addEventListener("click", () => {
+      item.scrollLeft -= containerWidth + 200;
+    });
+  });
+  
